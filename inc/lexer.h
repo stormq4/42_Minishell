0798aea2dc72bddf7bcff0a19fd6b9a3d4aafa4f
Storm De quaij <sde-quai@f0r1s13.codam.nl>
2022-03-03 17:32:06 +0100

    final of today 3-3-22

---
+++ inc/lexer.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   lexer.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sde-quai <sde-quai@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/03/03 17:25:03 by sde-quai      #+#    #+#                 */
+/*   Updated: 2022/03/03 17:29:27 by sde-quai      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+
+#endif
